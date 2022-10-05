d4f879a921dcdcfd072eae094e4e3514beb280ec
paul-yuk <shyuk@madup.com>
2022-10-05 15:28:10 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 import time
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-DEF_VERSION = 1
+
+DEF_VERSION = 2
+
 
 def main():
     while True:
         time.sleep(1)
-        print(f"version-{DEF_VERSION}")
+        logging.info(f"version-{DEF_VERSION}")
 
 
 if __name__ == "__main__":
